6d6035ec294d5c8d532bd50eb0781649a28103e1
Azamat <BzhukovAA@gmail.com>
2023-01-17 23:20:13 +0300

    recipes + recipes / {id} + tags + tags/ {id} + ingredients+ ingredients / {id}+ recipes (delete, update, list) + subscribes (list, add, destroy) + favorite_recipes (add,destroy) + front +

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
 
 #CORS_URLS_REGEX = r"^/api/.*$"
 #CORS_ALLOW_CREDENTIALS = True
-C   ORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://localhost',
     'http://127.0.0.1',
