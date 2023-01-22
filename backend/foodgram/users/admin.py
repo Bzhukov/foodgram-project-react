cde1323fc5cd9dca6ee3301bd32abd5ade2160d3
Azamat <BzhukovAA@gmail.com>
2023-01-22 18:11:31 +0300

    static

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -5,8 +5,13 @@ from django.contrib.auth.admin import UserAdmin
 User = get_user_model()
 
 
-class CustomAdmin(UserAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    list_editable = ('first_name', 'last_name', 'email',)
+    ordering = ('first_name','last_name',)
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('email', 'first_name', 'last_name')
+    empty_value_display = '-пусто-'
 
 
-admin.site.register(User)
+
+admin.site.register(User, CustomUserAdmin)
