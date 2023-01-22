11203d2cdf66db4b588d48c87b6da20ce6cbc1ea
Azamat <BzhukovAA@gmail.com>
2023-01-22 15:38:39 +0300

    recipes + recipes / {id} + tags + tags/ {id} + ingredients+ ingredients / {id}+ recipes (delete, update, list) + subscribes (list, add, destroy) + favorite_recipes (add,destroy) + front +

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from djoser.serializers import UserSerializer
+from rest_framework import permissions
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 from users.serializers import GetTokenSerializer
@@ -16,7 +18,7 @@ class CustomAuthToken(ObtainAuthToken):
     Получение токена в обмен на email и password.
     Права доступа: Доступно без токена.
     """
-    filter_backends = (LimitOffsetPagination,)
+    http_method_names = ['post', ]
 
     def post(self, request, *args, **kwargs):
         serializer = GetTokenSerializer(data=request.data)
@@ -32,4 +34,23 @@ class CustomAuthToken(ObtainAuthToken):
         else:
             return Response({'password': 'Введен неверный пароль', },
                             status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomUserViewSet(viewsets.ViewSet):
+    """
+     Список пользователей.
+     Права доступа: Всем
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    http_method_names = ('get',)
+
+    def list(self, request):
+        serializer = UserSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(self.queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
