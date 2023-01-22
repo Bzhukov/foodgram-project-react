fc82ab725a6a6af74b27e9fac73945456433f5f1
Azamat <BzhukovAA@gmail.com>
2023-01-22 16:58:00 +0300

    recipes + recipes / {id} + tags + tags/ {id} + ingredients+ ingredients / {id}+ recipes (delete, update, list) + subscribes (list, add, destroy) + favorite_recipes (add,destroy) + front + user +

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -65,14 +65,21 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', default=None),
-        'USER': os.getenv('POSTGRES_USER', default=None),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
-        'HOST': os.getenv('DB_HOST', default=None),
-        'PORT': os.getenv('DB_PORT', default=None)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME', default=None),
+#         'USER': os.getenv('POSTGRES_USER', default=None),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
+#         'HOST': os.getenv('DB_HOST', default=None),
+#         'PORT': os.getenv('DB_PORT', default=None)
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -112,6 +119,13 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5,
+}
+
+DJOSER = {
+    'PERMISSIONS': {
+        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
+        'user_list': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
+    }
 }
 
 STATIC_URL = '/static/'
