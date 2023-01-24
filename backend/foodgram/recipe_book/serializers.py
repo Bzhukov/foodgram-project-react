fd5263930abfefb03fcdc23015f2828483496933
Azamat <BzhukovAA@gmail.com>
2023-01-24 21:14:04 +0300

    flake8

--- backend/foodgram/recipe_book/serializers.py
+++ backend/foodgram/recipe_book/serializers.py
@@ -4,11 +4,11 @@ from django.contrib.auth import get_user_model
 from django.core.files.base import ContentFile
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.shortcuts import get_object_or_404
-from foodgram.settings import MAX_INGREDIENT_AMOUNT, MIN_INGREDIENT_AMOUNT
 from recipe_book.models import (Favorite, Ingredient, Recipe, ShoppingCart,
                                 Structure, Subscription, Tag)
-from rest_framework.exceptions import ValidationError
 from rest_framework import serializers, status
+from rest_framework.exceptions import ValidationError
+from foodgram.settings import MAX_INGREDIENT_AMOUNT, MIN_INGREDIENT_AMOUNT
 
 User = get_user_model()
 
