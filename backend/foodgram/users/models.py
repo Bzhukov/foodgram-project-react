321ec11aea031c4f6769e1eee48b8e159a4f8a6a
Azamat <BzhukovAA@gmail.com>
2023-01-24 21:02:15 +0300

    edit 2

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     email = models.EmailField(
         error_messages={'unique': 'Пользователь с таким email уже существует'},
